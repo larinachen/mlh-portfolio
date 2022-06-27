0088126624a5e35ee47ce73ea51f670327572342
Larina Chen <larinachen2024@u.northwestern.edu>
2022-06-26 23:09:07 -0700

    connect app to mysql database. implement get and post endpoints for timeline feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,68 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
+# loading environment variables in .env
 load_dotenv()
+
+# app configurations
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
 
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+
+# create POST endpoint
+@app.route('/api/timeline_post', methods=['POST'])
+def post_timeline_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    print([name,email,content,timeline_post])
+    print("hello")
+
+    return model_to_dict(timeline_post)
+
+# create GET endpoint
+@app.route('/api/timeline_post', methods=['GET'])
+def get_timeline_post():
+    return {
+        'timeline_posts':[
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+# create a DELETE endpoint
+@app.rount('api/timeline_post', methods=['POST'])
+def delete_timeline_post():
+    pass
+    # TODO: research how to delete with mysql
+
+# ======================= FRONT END ============================
 
 # variables
 class Hobby:
