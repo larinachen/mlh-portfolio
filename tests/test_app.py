2503753fcb13b58eb9dd81a20b5253bed4c35dae
Juan Escalada <juanescalada175@gmail.com>
2022-06-30 10:46:48 -0700

    Add timeline api POST tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,15 +12,37 @@ class AppTestCase(unittest.TestCase):
         response = self.client.get("/")
         assert response.status_code == 200
         html = response.get_data(as_text=True)
-        assert "<title>Home</title>" in html
-        # TODO add more tests for home page
+        assert '<title>Home</title>' in html
+        assert 'Yundi Chen' in html
+        assert '<img src="../static/img/Chen_Larina_Photo.jpg">' in html
 
-    def test_timeline(self):
+    def test_timeline_get(self):
         response = self.client.get("/api/timeline_post")
         assert response.status_code == 200
         assert response.is_json
         json = response.get_json()
         assert "timeline_posts" in json
         assert len(json["timeline_posts"]) == 0
+    
+    def test_timeline_post(self):
+        response = self.client.post('/api/timeline_post', data={
+            "name": "John Doe",
+            "email": "johndoe@email.com",
+            "content": "Testing from test_app.py!"
+        })
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert json['name'] == 'John Doe'
+        assert json['email'] == 'johndoe@email.com'
+        assert json['content'] == 'Testing from test_app.py!'
+
+        response_get = self.client.get("/api/timeline_post")
+        assert response_get.status_code == 200
+        assert response_get.is_json
+        json_get = response_get.get_json()
+        assert "timeline_posts" in json_get
+        assert len(json_get["timeline_posts"]) == 1
+        
         # TODO add more tests relating to the /api/timeline_post GET and POST APIs
         # TODO add more tests relating to the timeline page
